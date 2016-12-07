2f9a3fc064d6e4ca9e710cdec4425be2fd4eeff1
bobm8 <bobman1959@gmail.com>
2016-12-08 03:49:43 +1100

    changed and fixed alot of HTML

--- Warframe/src/Warframe/Controllers/HomeController.cs
+++ Warframe/src/Warframe/Controllers/HomeController.cs
@@ -13,17 +13,8 @@ namespace Warframe.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Abilitys()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
@@ -31,5 +22,6 @@ namespace Warframe.Controllers
         {
             return View();
         }
+
     }
 }
