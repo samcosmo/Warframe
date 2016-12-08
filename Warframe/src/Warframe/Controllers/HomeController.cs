ec918e007a0a4bd2b2c00419f256e4e65590e1bd
samcosmo <96cosmo@gmail.com>
2016-12-08 23:20:10 +1100

    Spaced out the controller

--- Warframe/src/Warframe/Controllers/HomeController.cs
+++ Warframe/src/Warframe/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace Warframe.Controllers
 {
     public class HomeController : Controller
     {
+        /*
+         * Index
+         */
+
         public IActionResult Index()
         {
             List<Models.Link> names = new List<Models.Link>();
@@ -29,48 +33,63 @@ namespace Warframe.Controllers
             return View();
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        /*
+         * Ability
+         */
 
         public IActionResult Ability()
         {
             return View();
         }
 
+        /*
+         * Mod
+         */
+
         public IActionResult Mod()
         {
             return View();
         }
+
+        /*
+         * Arcane
+         */
 
         public IActionResult Arcane()
         {
             return View();
         }
 
+        /*
+         * ArcaneHelmet
+         */
+
         public IActionResult ArcaneHelmet()
         {
             return View();
         }
+
+        /*
+         * Warframe
+         */
 
         public IActionResult Warframe()
         {
             return View();
         }
 
+        /*
+         * Weapon
+         */
+
         public IActionResult Weapon()
         {
             return View();
         }
+
+        /*
+         * Error
+         */
 
         public IActionResult Error()
         {
